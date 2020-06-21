6737aeda37507acfead44b0afc1e42a77bc566be
jonasruiz <33689667+jonasruiz@users.noreply.github.com>
2020-06-20 23:13:15 -0300

    novo

---
+++ obconn.js
@@ -0,0 +1,20 @@
+var dbname ="glpi"
+var dbuser ="root"
+var dbserver="localhost"
+var dbpwd = "3tech"
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(dbname,dbuser, dbpwd,{
+    host: dbserver,
+    dialect:'mysql'
+
+
+})
+
+sequelize.authenticate().then(function(){
+    console.log("Conectado com sucesso")
+}). catch(function(erro){
+   
+
+    console.log("Falha na conexão ao banco  " + dbname + "  no servidor  " + dbserver + " " + erro)
+})
